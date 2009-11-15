0914870a92b109887fabda7229313d23b9b3b873
Chris Carlisle <carlic578@gmail.com>
2009-11-15 02:03:16 -0600

    Added tranformations and ambient light.
    This is a big step forward on the progress of the ray tracer. I now have ambient
    lighting and the ability to have multiple spheres with different colors and have
    them transformed ^^. Need to finish lights, reflection, and refraction.

--- graphics/lab6/skeleton.c
+++ graphics/lab6/skeleton.c
@@ -27,7 +27,7 @@ int numObjs; //number of objects
 ******************************************/
 struct {
     int size;
-    float d;
+    GLdouble d;
  } 
 view;
 
@@ -44,7 +44,7 @@ RGBColor backgroundColor = {0,0,0}; //background color for the scene
 typedef struct {
     Vector position;
     RGBColor color;
-    float shininess;
+    GLdouble shininess;
 } 
 LIGHT;
 
@@ -56,7 +56,7 @@ LIGHT;
 typedef struct {
     RGBColor diffuse;
     RGBColor specular;
-    float shininess;
+    GLdouble shininess;
 } 
 MATERIAL;
 
@@ -73,11 +73,26 @@ typedef struct {
 } 
 OBJECT;
 
+/************************************************
+|| Ray strcut
+|| Purpose: holds a ray's vectors
+************************************************/
+typedef struct {
+	Vector u; //starting point
+	Vector v; //direction
+}
+Ray;
+
+typedef struct {
+	int objNumber;
+	Vector intPoint;
+}
+ObjectAttributes;
+	
 MATERIAL currentMaterial; //current material
 Transform currentTransform[MAX_GROUPS]; //array of transformations for groups
 Transform tempSave; //holds a tranform temporarily off to the side
 int curGroupLevel=0; //holds the current group level
-RGBColor Pixel[winWidth][winHeight];
 
 OBJECT objects[MAX_OBJECTS]; //array of objects
 LIGHT lightSources[MAX_LIGHTS]; //array of lights
@@ -114,7 +129,7 @@ void readFile(char *fname)
       {
 	  fscanf(SceneFile, "%d", &view.size);
 	  //printf("View size: %d\n", view.size);
-	  fscanf(SceneFile, "%f", &view.d);
+	  fscanf(SceneFile, "%lf", &view.d);
 	  //printf("View distance to corner of image plane: %f\n", view.d);
       }
       else if (!strncmp(cmd, "#", 1)) 
@@ -188,7 +203,7 @@ void readFile(char *fname)
 	  fscanf(SceneFile, "%lf", &currentMaterial.specular.red);
 	  fscanf(SceneFile, "%lf", &currentMaterial.specular.green);
 	  fscanf(SceneFile, "%lf", &currentMaterial.specular.blue);
-	  fscanf(SceneFile, "%f", &currentMaterial.shininess);
+	  fscanf(SceneFile, "%lf", &currentMaterial.shininess);
 	  // What do I do with this?
 	  // you need to put this into the object
       }
@@ -266,74 +281,276 @@ void readFile(char *fname)
 
   fclose(SceneFile); //close the scene file
 }
-		
-void findIntersections()
+
+Ray ray_from_eye_through(Vector curPixel)
 {
-	int t1,t2,a,b,c,row,col;
-	Vector u,v, curPixel;
+	Ray tempRay;
 
-	//get u  which is the camera position
-	u.x = 0;
-	u.y = 0;
-	u.z = 1;
-	u.w = 1;
+	//Camera
+	tempRay.u.x = 0;
+	tempRay.u.y = 0;
+	tempRay.u.z = 1;
+	tempRay.u.w = 1;
 
-	//find coord for current pixel
-	for(row=0; row < view.d; row++)
-		for(col=0; col < view.d; col++)
-		{
-			curPixel.x = -d + (d/n) + ((2d/n) * col);
-			curPixel.y = d - (d/n) - ((2d/n) * row);
-			curPixel.z = 0;
-			curPixel.w = 1;
-
-
-	
-			//find v
-			v=unit_vector(vector_subtract(curPixel - u));
-
-			//store values for a b c
-			a = dot_product(u, u);
-			b = 2 * dot_product(u,v);
-			c = dot_product(u,u) -1;
-
-			//Ray/Sphere intersection
-			if(b*b > 4*a*c)
-			{
-				if(b >0)
-					t1 = (-b - sqrt(b^2 - 4*a*c))/(2*a);
-				else
-					t1 = (-b + sqrt(b^2 - 4*a*c))/(2*a);
-				t2 = c / (a*t1);
-			}
-		}
+	tempRay.v=vector_subtract(curPixel , tempRay.u);
+	printf("Vector\nX: %f, Y: %f, Z: %f\n", tempRay.v.x, tempRay.v.y, tempRay.v.z);
+	tempRay.v=unit_vector(vector_subtract(curPixel , tempRay.u));
+	printf("\nUnit Vector\nX: %f, Y: %f, Z: %f\n", tempRay.v.x, tempRay.v.y, tempRay.v.z);
+	//printf("\nX: %f, Y: %f, Z: %f\n", tempRay.v.x, tempRay.v.y, tempRay.v.z);
+	return tempRay;
 }
 
-void drawPixels()
+RGBColor Shade(ObjectAttributes obj, Ray ray)
 {
-	int winWidth = view.size;
-	int winHeight = view.size;
+	RGBColor color; 
+	int iNum;
+
+	//store the object number
+	iNum = obj.objNumber;
+
+
+
+	//Color = black
+	color.red = ambient_light.red * objects[iNum].material.diffuse.red;
+	color.green = ambient_light.green * objects[iNum].material.diffuse.green;
+	color.blue = ambient_light.blue * objects[iNum].material.diffuse.blue;
+
+	return color;
+
+}
+
+Ray moveRay(Ray tempRay, Matrix inverse)
+{
+	//inverse = transposeMatrix(inverse);
+
+	tempRay.u = matrixTimesVector(inverse, tempRay.u);
+	tempRay.v = matrixTimesVector(inverse, tempRay.v);
+
+	return tempRay;
+}
+
+ObjectAttributes closest_intersection(Ray ray)
+{
+	int i, smallestObj;
+	Ray currRay;
+	ObjectAttributes obj; //object to return values
+	double a, //a for quad equation
+	       b, //b for quad equation
+	       c, //c for quad equation
+	       t, //smallest t for current obj
+	       t1, //t1 for current obj
+	       t2, //t2 for current obj
+	       smallestT=9000000.0; //smallest t
+
+
+	obj.objNumber = -1;
+	//For each object
+	for(i=0; i < numObjs; i++)
+	{
+
+		//Move current ray with inverse of current object 
+		//to see if it intersects with origin object
+		currRay = moveRay(ray, objects[i].transform.inverseTransformation);
+
+		//store values for a b c
+		a = dot_product(currRay.v, currRay.v);
+		b = 2 * dot_product(currRay.u,currRay.v);
+		c = dot_product(currRay.u,currRay.u) - 1;
+
+		printf("A: %lf, B: %lf, C: %lf\n",a,b,c);
+
+		t=0;
+		//Calculate t
+		if(b*b > 4*a*c)
+		{
+			printf("Checking for hit\n");
+			if(b >0)
+			{
+				printf("Greater than 0\n");
+				t1 = (-b - sqrt((b*b) - 4*a*c))/(2*a);
+			}
+			else
+				printf("Less than 0\n");
+				t1 = (-b + sqrt((b*b) - 4*a*c))/(2*a);
+
+			t2 = c / (a*t1);
+
+			//Find smallest t for current obj
+			if(t1 < t2 && t1 > 0)
+			{
+				t = t1;
+			}
+			if(t2 < t1 && t2 > 0)
+			{
+				t = t2;
+			}
+
+			printf("t1 is: %lf\n", t1);
+			printf("t2 is: %lf\n", t2);
+			printf("t is: %lf\n", t);
+
+			//If smallest obj keep track of it
+			if(t < smallestT && t > 0)
+			{
+				printf("Object #: %d\n", i+1);
+
+
+				smallestT = t;
+				printf("Smallest t is: %lf\n", smallestT);
+				obj.objNumber = i;
+
+			}
+		}
+
+	}//done with each object
+
+	//Calculate point of intersect
+	obj.intPoint = vector_add(ray.u, vector_X_n(ray.v, smallestT));
+		
+	//return object attributes
+	return obj;  //obj # with smallest t &  the point
+
+}
+
+//Trace one pixel
+RGBColor trace(Ray ray)
+{
+	ObjectAttributes obj;
+	obj = closest_intersection(ray); //get nearest obj location & #
+
+	printf("Object # for if is: %d\n", obj.objNumber);
+	if(obj.objNumber != -1)
+	{
+		printf("Return color");
+		return Shade(obj, ray);
+	}
+	else
+	{
+		printf("Return BG\n");
+		return backgroundColor;
+	}
+}
+
+void drawPixels(RGBColor Pixel[][view.size])
+{
 	FILE *picfile;
 	int j,i;
 	picfile = fopen("out.ppm", "w");
-	fprintf(picfile, "P6\n# %dx%d Raytracer output\n%d %d\n255\n",
-                view.size, view.size, view.size, view.size);
+	fprintf(picfile, "P6\n%d %d\n255\n", view.size, view.size);
 
         // For each pixel
 	for (j=view.size; j >= 0; j--)     // Y is flipped!
-	    for (i=0; i < view.size; i++) 
-	    {
-	        fprintf(picfile, "%lf%lf%lf", 
-				Pixel[i][j].red,Pixel[i][j].green,Pixel[i][j].blue);
+	    for (i=0; i < view.size; i++) {
+	        fprintf(picfile, "%c%c%c", 
+				(int) Pixel[i][j].red*255,(int)
+				Pixel[i][j].green*255,(int) Pixel[i][j].blue*255);
 		    // Remember though that this is a number between 0 and 255
 		    // so might have to convert from 0-1.
 	    }
+
+
+
+	for (j=view.size; j >= 0; j--)     // Y is flipped!
+	{
+		printf("Row %d\n",i);
+	    for (i=0; i < view.size; i++) {
+		printf("Col %d\n",j);
+	        printf("Red: %lf, Green: %lf, Blue: %lf\n", 
+				Pixel[i][j].red,Pixel[i][j].green,Pixel[i][j].blue);
+
+		  glBegin(GL_POINTS);
+		  glColor3f(Pixel[i][j].red, Pixel[i][j].green, Pixel[i][j].blue);
+		  glVertex2f(i, j);
+		  glEnd();
+	    }
+	}
+	
+
 }	
+
+
+void RayCast()
+{
+    	RGBColor Pixel[view.size][view.size]; //keep track of colors
+	int row,col;
+	GLdouble d,n;
+	RGBColor color;
+	Vector p,  //point of intersection 
+	       curPixel; //current pixel being calculated
+
+
+	d = view.d; 
+	n = view.size; //number of pixels
+	
+	//For each pixel
+    	printf("Entering pixel rows\n");
+	for(row=0; row < view.size; row++)
+	{
+		printf("\n\nEntering row %d\n", row);
+		for(col=0; col < view.size; col++)
+		{
+    			printf("\nEntering col %d\n", col);
+
+			//Finding the center of the current pixel
+    			printf("Finding x ");
+			curPixel.x = -d + (d/n) + (((2*d)/n) * row);
+    			printf("Finding y ");
+			curPixel.y = - d + (d/n) + (((2*d)/n) * col);
+    			printf("Placing z ");
+			curPixel.z = 0;
+    			printf("Placing w ");
+			curPixel.w = 1;
+
+			//Find color and store it
+    			printf("Entering trace\n");
+			color=	trace(ray_from_eye_through(curPixel));
+
+			Pixel[row][col].red = color.red;
+			Pixel[row][col].green = color.green;
+			Pixel[row][col].blue = color.blue;
+		}
+	}
+     drawPixels(Pixel); //draw the results
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+       	glMatrixMode(GL_MODELVIEW);
+       	glLoadIdentity();
+       	glFlush();
+       	glColor3f(0,0,0);
+
+       	printf("Casting rays\n");
+       	RayCast(); //start finding points
+	printf("Number of Objects: %d\n", numObjs);
+       	glutSwapBuffers();
+}
+
+void myinit()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glPointSize(1.0);
+	gluOrtho2D(0, view.size, 0, view.size);
+	glClearColor(0.0,0.0,0.0,0.0);
+
+}
 
 int main(int argc, char **argv)
 {
-    readFile(argv[1]); //read the scene
-    findIntersections(); //find the intersections
-     drawPixels(); //draw the results
+	//Read file in
+	printf("Reading file\n");
+       	readFile(argv[1]); //read the scene
+
+	//Get openGL display of results ready
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(view.size, view.size);
+	glutCreateWindow("ray tracer");
+	myinit(); //setup lights and window
+
+    glutDisplayFunc(display); //start drawing
+    glutMainLoop(); //start looping
 }
 
